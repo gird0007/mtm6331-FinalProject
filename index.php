2f63345db8bf1926bc15948929fa48f2251aa9e1
Mayank Girdhar <gird0007@algonquinlive.com>
2018-10-26 12:11:08 -0400

    working

--- index.php
+++ index.php
@@ -5,11 +5,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>SONAR | HOME</title>
 </head>
 
 <body>
-
+  <div class="wrapper">
+    <div class="hero">
+      <div class="logo">
+        <img src="" alt="">
+      </div>
+    </div>
+    <div class="big-buttons">
+      <div class="button">
+        <a href="#">find an artist</a>
+      </div>
+      <div class="button">
+        <a href="#">promote yourself</a>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
