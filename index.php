0528ed6d956f35dd2245a7570f1e40fde17606bd
Mayank Girdhar <gird0007@algonquinlive.com>
2018-10-31 16:56:36 -0400

    working on the design

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,7 +27,105 @@
         <a href="#">promote yourself</a>
       </div>
     </div>
+    <div class="sidebar-arrow" id="sidebar">
+      <img src="./img/open-arrow.png" alt="open-sidebar">
+      <img src="./img/close-arrow.png" alt="open-sidebar">
+    </div>
+    <div class="sidebar">
+      <div class="logo">
+        <img src="./img/9logo.png" alt="logo_placeholder">
+      </div>
+      <hr>
+      <div class="search"></div>
+      <h2>explore more genres</h2>
+      <ul>
+        <li><a href="#">genre 1</a></li>
+        <li><a href="#">genre 2</a></li>
+        <li><a href="#">genre 3</a></li>
+        <li><a href="#">genre 4</a></li>
+      </ul>
+      <div class="social">
+        <ul>
+          <li><a href="#"><img src="./img/footer_social/facebook.png" alt="facebook_logo" srcset=""></a></li>
+          <li><a href="#"><img src="./img/footer_social/instagram.png" alt="instagram_logo" srcset=""></a></li>
+          <li><a href="#"><img src="./img/footer_social/twitter.png" alt="twitter_logo" srcset=""></a></li>
+        </ul>
+      </div>
+    </div>
   </header>
+  <main>
+    <div class="banner">
+      <h1>featured artists</h1>
+    </div>
+    <article>
+      <h2>Genre</h2>
+      <div class="img-contain">
+        <img src="https://via.placeholder.com/350x150
+
+C/O https://placeholder.com/" alt="">
+      </div>
+      <h3>Lorem Ipsum</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium ut nesciunt aperiam voluptas perspiciatis saepe amet dolor.</p>
+      <div class="article-button">
+        <a href="#">view profile</a>
+      </div>
+      <p><a href="#">view more in this genre</a></p>
+    </article>
+    <article>
+      <h2>Genre</h2>
+      <div class="img-contain">
+        <img src="https://via.placeholder.com/350x150
+
+C/O https://placeholder.com/" alt="">
+      </div>
+      <h3>Lorem Ipsum</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium ut nesciunt aperiam voluptas perspiciatis saepe amet dolor.</p>
+      <div class="article-button">
+        <a href="#">view profile</a>
+      </div>
+      <p><a href="#">view more in this genre</a></p>
+    </article>
+    <article>
+      <h2>Genre</h2>
+      <div class="img-contain">
+        <img src="https://via.placeholder.com/350x150
+
+C/O https://placeholder.com/" alt="">
+      </div>
+      <h3>Lorem Ipsum</h3>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium ut nesciunt aperiam voluptas perspiciatis saepe amet dolor.</p>
+      <div class="article-button">
+        <a href="#">view profile</a>
+      </div>
+      <p><a href="#">view more in this genre</a></p>
+    </article>
+  </main>
+  <hr>
+  <footer>
+    <div class="section">
+      <ul>
+        <li><a href="#">site map</a></li>
+        <li><a href="#">faq</a></li>
+        <li><a href="#">legal</a></li>
+        <li><a href="#">privacy</a></li>
+      </ul>
+    </div>
+    <div class="section">
+      <ul>
+        <li><a href="#"><img src="./img/footer_social/facebook.png" alt="facebook_logo" srcset=""></a></li>
+        <li><a href="#"><img src="./img/footer_social/instagram.png" alt="instagram_logo" srcset=""></a></li>
+        <li><a href="#"><img src="./img/footer_social/twitter.png" alt="twitter_logo" srcset=""></a></li>
+      </ul>
+    </div>
+    <div class="section">
+      <div class="one">
+        <img src="./img/9logo.png" alt="logo">
+      </div>
+      <div class="two">
+        <p>&copy; Lorem, ipsum. 2018</p>
+      </div>
+    </div>
+  </footer>
   </div>
 </body>
 
