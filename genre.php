cab3b4e18b959c6222920d7de6384fef96bd12b1
Mayank Girdhar <gird0007@algonquinlive.com>
2018-11-02 01:53:00 -0400

    genre page completed design

---
+++ genre.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SONAR | GENRE</title>
+  <link rel="stylesheet" href="./css/style.css">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=KoHo');
+  </style>
+</head>
+<body>
+  <div class="wrapper">
+    <header class="genre-header">
+      <div class="hero">
+        <div class="genre-name">
+          <h1>genre 1</h1>
+        </div>
+      </div>
+      <div class="sidebar-container" id="side-contain">
+        <div class="sidebar-arrow" id="sidebar">
+          <img src="./img/open-arrow.png" alt="open-sidebar" class="arrow open" id="open">
+          <img src="./img/close-arrow.png" alt="close-sidebar" class="arrow close" id="close">
+        </div>
+      <div class="sidebar" id="sidebar-box">
+      <div class="user">
+        <div class="account-icon">
+          <img src="./img/assets/account-icon.png" alt="account-icon">
+        </div>
+        <p>name</p>
+        <div class="setting-icon">
+          <img src="./img/assets/settings-icon.png" alt="settings-icon">
+        </div>
+      </div>
+        <div class="logo">
+          <img src="./img/9logo.png" alt="logo_placeholder">
+        </div>
+        <hr>
+        <div class="search"></div>
+        <h2>explore more genres</h2>
+        <ul>
+          <li><a href="#">genre 1</a></li>
+          <li><a href="#">genre 2</a></li>
+          <li><a href="#">genre 3</a></li>
+          <li><a href="#">genre 4</a></li>
+        </ul>
+        <div class="social">
+          <ul>
+            <li><a href="#"><img src="./img/footer_social/facebook.png" alt="facebook_logo" srcset=""></a></li>
+            <li><a href="#"><img src="./img/footer_social/instagram.png" alt="instagram_logo" srcset=""></a></li>
+            <li><a href="#"><img src="./img/footer_social/twitter.png" alt="twitter_logo" srcset=""></a></li>
+          </ul>
+        </div>
+        <div class="back-to-home">
+          <p><a href="index.php">back to home</a></p>
+        </div>
+      </div>
+    </header>
+    <main class="genre-main">
+      <article>
+        <div class="artist">
+          <div class="artist-img">
+            <img src="https://via.placeholder.com/150
+
+C/O https://placeholder.com/" alt="">
+          </div>
+          <div class="artist-info">
+            <h1>artist name</h1>
+            <h2>location</h2>
+            <h3>genre/subgenre</h3>
+          </div>
+        </div>
+        <div class="artist">
+          <div class="artist-img">
+            <img src="https://via.placeholder.com/150
+
+C/O https://placeholder.com/" alt="">
+          </div>
+          <div class="artist-info">
+            <h1>artist name</h1>
+            <h2>location</h2>
+            <h3>genre/subgenre</h3>
+          </div>
+        </div>
+      </article>
+      <article>
+        <div class="artist">
+          <div class="artist-img">
+            <img src="https://via.placeholder.com/150
+
+C/O https://placeholder.com/" alt="">
+          </div>
+          <div class="artist-info">
+            <h1>artist name</h1>
+            <h2>location</h2>
+            <h3>genre/subgenre</h3>
+          </div>
+        </div>
+        <div class="artist">
+          <div class="artist-img">
+            <img src="https://via.placeholder.com/150
+
+C/O https://placeholder.com/" alt="">
+          </div>
+          <div class="artist-info">
+            <h1>artist name</h1>
+            <h2>location</h2>
+            <h3>genre/subgenre</h3>
+          </div>
+        </div>
+      </article>
+      <article>
+        <div class="artist">
+          <div class="artist-img">
+          <img src="https://via.placeholder.com/150
+
+C/O https://placeholder.com/" alt="">
+          </div>
+          <div class="artist-info">
+            <h1>artist name</h1>
+            <h2>location</h2>
+            <h3>genre/subgenre</h3>
+          </div>
+        </div>
+        <div class="artist">
+          <div class="artist-img">
+            <img src="https://via.placeholder.com/150
+
+C/O https://placeholder.com/" alt="">
+          </div>
+          <div class="artist-info">
+            <h1>artist name</h1>
+            <h2>location</h2>
+            <h3>genre/subgenre</h3>
+          </div>
+        </div>
+      </article>
+    </main>
+    <hr>
+    <footer>
+    <div class="section">
+      <ul>
+        <li><a href="#">site map</a></li>
+        <li><a href="#">faq</a></li>
+        <li><a href="#">legal</a></li>
+        <li><a href="#">privacy</a></li>
+      </ul>
+    </div>
+    <div class="section">
+      <ul>
+        <li><a href="#"><img src="./img/footer_social/facebook.png" alt="facebook_logo" srcset=""></a></li>
+        <li><a href="#"><img src="./img/footer_social/instagram.png" alt="instagram_logo" srcset=""></a></li>
+        <li><a href="#"><img src="./img/footer_social/twitter.png" alt="twitter_logo" srcset=""></a></li>
+      </ul>
+    </div>
+    <div class="section">
+      <div class="one">
+        <img src="./img/9logo.png" alt="logo">
+      </div>
+      <div class="two">
+        <p>&copy; Lorem, ipsum. 2018</p>
+      </div>
+    </div>
+  </footer>
+  </div>
+  <script src="./js/index.js"></script>
+</body>
+</html>
